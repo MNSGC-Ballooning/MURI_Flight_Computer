bdddfc27492fac81687dcf870758a84cd5299e6b
asifally <asifally004@gmail.com>
2019-06-06 11:19:20 -0500

    Added the usage of melons library

--- unitTests/altimeterTest/altimeterTest.ino
+++ unitTests/altimeterTest/altimeterTest.ino
@@ -1,65 +1,45 @@
 // Author: Asif ALly
-#include <MS5xxx.h>
+#include <Melon_MS5607.h>
 
-MS5xxx MS5(&Wire);
-int ms_temp=0;
-int ms_pressure = 0;
+Melon_MS5607 MS5=Melon_MS5607();
+int32_t tempByte;
+int32_t presByte;
+unsigned long prevtime = 0;
+float temp;
+float pres;
 
+#define MS_I2C_ADDR 0xEE // 11101110
 
 void setup() {
   Serial.begin(9600);
-  MS5.connect();
+  MS5.begin(MS_I2C_ADDR);
   delay(500);
 
 }
 
-//String create8bit(String binval){
-//  int len = binval.length();
-//  int idx=0;int idx2=0;
-//  String largebinval = "0000000"+binval;
-//  int fullLen = largebinval.length();
-//  String bin8bitval = "00000000";
-//  for(idx=fullLen-7;idx<fullLen;idx++){
-//    bin8bitval[idx2] = largebinval[idx];idx2++;
-//  }
-//  return bin8bitval;
-//}
-
 void ms5Update() {
-  MS5.ReadProm();
-  MS5.Readout();
-  ms_temp = (MS5.GetTemp()/100); //because temp is given in .01 C
-  ms_pressure =(MS5.GetPres());
-  String data = "";
-  data += (String(ms_temp)+ "," + String(ms_pressure) + ",");
-  Serial.println(data);
+  
+  if(MS5.readTemperature()){
+    
+    tempByte=MS5.getTemperature(); 
+
+    }
+  if(MS5.readPressure()){
+    presByte= MS5.getPressure();
+  }
+
+  Serial.println("pressure: " +  String(presByte));
+  Serial.println("temp: " + String(tempByte));
+
+  
+//  String data = "";
+//  data += ("Temp:" + String(ms_temp)+ "," + " Pressure:" + String(ms_pressure) + ",");
+//  Serial.println(data);
 }
 
 void loop() {
+  if (millis()-prevtime>=1){
+    prevtime=millis();
   ms5Update();
-
+  }
 }
-
-//float MS5607::getPressure() {
-//  
-//    if(rawTemperature && rawPressure != 0) {
-//        int64_t dT = rawTemperature - (calData[4] << 8);
-//        int64_t OFF  = (calData[1] << 17) + ((dT * calData[3]) >> 6);
-//        int64_t SENS = (calData[0] << 16) + ((dT * calData[2]) >> 7);
-//        return ((((((rawPressure * SENS) >> 21) - OFF) >> 15) / 100.0) + 20) * 0.02953;
-//    }
-//    else {
-//        return -1;
-//    }
-//}
-//
-//float MS5607::getTemperature() {
-//
-//    if(rawTemperature && rawPressure != 0) {
-//        int64_t dT = rawTemperature - (calData[4] << 8);
-//        return (((2000 + ((dT * calData[5]) >> 23)) / 100.0) * 1.8) + 32;
-//    }
-//    else {
-//        return NULL;
-//    }
-//}
